62f866df2219cafb93da34269e52e017428f5911
FroggyRiseman <froggyriseman@gmail.com>
2015-04-22 11:41:39 +0200

    Edited the structure of the if statement in tango_with_django_project/urls.py

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
 if settings.DEBUG:
     urlpatterns += patterns(
             'django.views.static',
-            (r'^media/(?P<path>.*)',
-            'serve',
-            {'document_root': settings.MEDIA_ROOT}),
+            (r'^media/(?<P<path>.*)',
+                'serve',
+                {'document_root': settings.MEDIA_ROOT}),
     )
