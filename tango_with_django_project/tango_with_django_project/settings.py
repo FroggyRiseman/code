8d851d0075be39223cf971505610a00eb600bee4
FroggyRiseman <froggyriseman@gmail.com>
2015-04-22 11:48:11 +0200

    Added MEDIA_URL and MEDIA_ROOT to settings.py

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -93,3 +93,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
         STATIC_PATH,
 )
+
+# Media files
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Absoulte path to the media directory
