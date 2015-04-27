f80d08a7df2ed760939ae647ba0032dea38083cd
FroggyRiseman <froggyriseman@gmail.com>
2015-04-27 14:06:20 +0200

    Changed fields to list_display

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
-admin.site.register(Page)
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('views', 'likes')
 
+admin.site.register(Category, CategoryAdmin)
+    
 class PageAdmin(admin.ModelAdmin):
-    fields = ['category', 'title', 'url']
+    list_display = ('category', 'title', 'url')
 
 admin.site.register(Page, PageAdmin)
