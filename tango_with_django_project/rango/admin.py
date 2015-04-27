ea7df15f325b1ab3caa5184fe3d4072b7687e005
FroggyRiseman <froggyriseman@gmail.com>
2015-04-27 13:25:31 +0200

    Added the classes CategoryAdmin and PageAdmin

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -3,3 +3,15 @@ from rango.models import Category, Page
 
 admin.site.register(Category)
 admin.site.register(Page)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    fields = ['views', 'likes']
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class PageAdmin(admin.ModelAdmin):
+    fields ['category', 'title', 'url']
+
+admin.site.register(Page, PageAdmin)
