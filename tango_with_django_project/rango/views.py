0e86f6513d0a507093fc5868f74a0327504bb00a
FroggyRiseman <froggyriseman@gmail.com>
2015-05-11 10:59:18 +0200

    Added category() view to views.py

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -10,3 +11,17 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category_name'] = category.name
+        page = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
