e7511f8788a6427d0bb0f23dc794ce3505d69db5
FroggyRiseman <froggyriseman@gmail.com>
2015-05-20 17:34:56 +0200

    Edited the pages in category() to display by amount of views descending.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -26,7 +26,7 @@ def category(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
-        pages = Page.objects.filter(category=category)
+        pages = Page.objects.filter(category=category).order_by('-views')
         context_dict['pages'] = pages
         context_dict['category'] = category
     except Category.DoesNotExist:
