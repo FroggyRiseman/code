c498c7a1a105fd21338bfedf28629821292b8d17
FroggyRiseman <froggyriseman@gmail.com>
2015-06-01 10:26:03 +0200

    Edited views.py

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -21,7 +21,7 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'rango/about.html')
+    return render(request, 'rango/about.html', {})
 
 
 def category(request, category_name_slug):
@@ -142,7 +142,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
 
 
 @login_required
