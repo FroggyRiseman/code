98f73375e45afcae5d5fd42a498f1512f4aa3913
FroggyRiseman <froggyriseman@gmail.com>
2015-06-03 15:25:33 +0200

    Updated the index() function with the sessions based cookies.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -17,31 +17,33 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
-    visits = int(request.COOKIES.get('visits', '1'))
 
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
     reset_last_visit_time = False
-    response = render(request, 'rango/index.html', context_dict)
-    
-    if 'last_visit' in request.COOKIES:
-        last_visit = request.COOKIES['last_visit']
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
         last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
 
-        if (datetime.now() - last_visit_time).days > 0:
-            visits = visits + 1
+        if (datetime.now() - last_visit_time).seconds > 0:
+            visits = visits +1
             reset_last_visit_time = True
 
     else:
         reset_last_visit_time = True
-        context_dict['visits'] = visits
-        response = render(request, 'rango/index.html', context_dict)
 
     if reset_last_visit_time:
-        response.set_cookie('last_visit', datetime.now())
-        response.set_cookie('visits', visits)
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+    response = render(request, 'rango/index.html', context_dict)
 
     return response
 
-
+    
 def about(request):
     return render(request, 'rango/about.html', {})
 
