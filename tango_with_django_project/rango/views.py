3b371d80d9c4dca3aba5f89c12696c218ab27f80
FroggyRiseman <froggyriseman@gmail.com>
2015-06-03 10:51:07 +0200

    Edited a syntax error in views.py

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -24,7 +24,7 @@ def index(request):
     
     if 'last_visit' in request.COOKIES:
         last_visit = request.COOKIES['last_visit']
-        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%:S")
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
 
         if (datetime.now() - last_visit_time).days > 0:
             visits = visits + 1
